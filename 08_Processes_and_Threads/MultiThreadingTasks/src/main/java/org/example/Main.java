5adcd19d93bf8ffd299c27a1720ae558022892ef
Эльнара Габдрахманова <elnaradin@gmail.com>
2023-09-30 13:44:16 +0500

    task 2

--- 08_Processes_and_Threads/MultiThreadingTasks/src/main/java/org/example/Main.java
+++ 08_Processes_and_Threads/MultiThreadingTasks/src/main/java/org/example/Main.java
@@ -1,10 +1,21 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        var counter = new Service();
-        counter.log();
-        counter.debug();
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        var counter = new Service();
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            Thread thread = new Thread(() -> IntStream.range(0, 10).forEach((j) -> counter.log()), "T" + i);
+            threads.add(thread);
+        }
+        threads.forEach(Thread::start);
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        counter.debug();
     }
 }

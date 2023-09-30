ea556631b87c00b3d1527db33b618a80493be038
Эльнара Габдрахманова <elnaradin@gmail.com>
2023-09-30 14:05:15 +0500

    task 3

--- 08_Processes_and_Threads/MultiThreadingTasks/src/main/java/org/example/Service.java
+++ 08_Processes_and_Threads/MultiThreadingTasks/src/main/java/org/example/Service.java
@@ -1,14 +1,18 @@
 package org.example;
 
 import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Service {
     private Integer count = 0;
     private final LinkedList<String> log = new LinkedList<>();
-    public synchronized void log(){
-        count = count + 1;
-        String name = Thread.currentThread().getName();
-        log.add(name);
+    private final Lock lock = new ReentrantLock();
+    public void log(){
+        lock.lock();
+        count += 1;
+        log.add(Thread.currentThread().getName());
+        lock.unlock();
     }
     public void debug(){
         System.out.println(count);

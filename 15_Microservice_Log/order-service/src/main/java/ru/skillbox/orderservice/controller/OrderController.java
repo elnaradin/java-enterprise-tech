12291b0c1db045e1f6b827e1c6df18d272d08abe
Эльнара Габдрахманова <elnaradin@gmail.com>
2023-10-11 21:19:53 +0500

    task 2

--- 15_Microservice_Log/order-service/src/main/java/ru/skillbox/orderservice/controller/OrderController.java
+++ 15_Microservice_Log/order-service/src/main/java/ru/skillbox/orderservice/controller/OrderController.java
@@ -2,7 +2,12 @@ package ru.skillbox.orderservice.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import ru.skillbox.orderservice.domain.Order;
 import ru.skillbox.orderservice.domain.OrderDto;
 import ru.skillbox.orderservice.domain.StatusDto;
